ba98b3fa64981599f60312a169be06ab81e02ba7
dimagoryak <dimagoryak@gmail.com>
2016-10-24 12:09:26 +0300

    indexchange

--- views/products/index.php
+++ views/products/index.php
@@ -11,38 +11,110 @@ $this->params['breadcrumbs'][] = $this->title;
 <link href="/../views/products/style/style.css" rel="stylesheet" type="text/css" >
 <div class="site-about">
     <!--Type code here-->
-    
-    <table>
-        <thead>
-        <th>
-            <td>Назва</td>
-            <td>Ціна</td>
-            <td>ата створення</td>
-        </th>
-        </thead>
-        <tbody>
 
+<!--    <table>-->
+<!--        <thead>-->
+<!--        <th>-->
+<!--            <td>Назва</td>-->
+<!--            <td>Ціна</td>-->
+<!--            <td>Дата створення</td>-->
+<!--        </th>-->
+<!--        </thead>-->
+<!--        <tbody>-->
+<!---->
+<!---->
+<!--    --><?php
+//        foreach($data['products'] as $product){
+//            ?>
+<!--            <tr>-->
+<!--                <td>--><?php
+//                    foreach($data['producer'] as $producer){
+//                        if($producer['id_producer']==$product['maker']){
+//                            echo $producer['name'];
+//                            break;
+//                        }
+//                    }
+//                    echo " ".$product['version'];
+//                    ?><!--</td>-->
+<!--                <td>--><?php //echo $product['price'];?><!--</td>-->
+<!--                <td>--><?php //echo date("dd mm yy",$product['price']);?><!--</td>-->
+<!--            </tr>-->
+<!--    --><?php
+//        }
+//    ?>
+<!--        </tbody>-->
+<!--    </table>-->
 
-    <?php
-        foreach($data['products'] as $product){
-            ?>
-            <tr>
-                <td><?php
-                    foreach($data['producer'] as $producer){
-                        if($producer['id_producer']==$product['maker']){
-                            echo $producer['name'];
-                            break;
-                        }
-                    }
-                    echo " ".$product['version'];
-                    ?></td>
-                <td><?php echo $product['price'];?></td>
-                <td><?php echo date("dd mm yy",$product['price']);?></td>
-            </tr>
-    <?php
+<h3>FEATURED</h3>
+    <style type="text/css">
+        p{	margin-left: 4px;
+            margin-right: 4px;
+
         }
-    ?>
-        </tbody>
-    </table>
-    Index
+        img{
+            margin-left: 4px;
+            margin-right: 4px;
+        }
+        #mainbutton{
+            width: 38%;
+        }
+        #secondbutton{
+            width: 20%;
+        }
+        #thirdbutton{
+            width: 35%;
+        }
+    </style>
+
+
+    <div class="col-xs-6 col-sm-8 col-lg-12">
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Add to cart</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">Compare</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+    </div>
+
+
+
+
 </>
