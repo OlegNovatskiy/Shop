4389e3bbebf6e7e805c03387c825cb78dacd69ff
StarTraveler97 <Crusades@meta.ua>
2016-10-24 12:06:38 +0300

    load category 1

--- views/products/category.php
+++ views/products/category.php
@@ -8,8 +8,168 @@ use yii\helpers\Html;
 $this->title =  $data['header_title'];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<link href="/../views/category/style/style.css" rel="stylesheet" type="text/css" >
-<div class="site-about">
+<link href="views/shop/style/style.css" rel="stylesheet" type="text/css" >
+
     <!--Type code here-->
-    Category
+
+<style type="text/css">
+	p{	margin-left: 4px;
+	margin-right: 4px;
+		
+	}
+	img{
+		margin-left: 4px;
+	margin-right: 4px;
+	}
+	#mainbutton{
+		width: 50%;
+	}
+	#secondbutton{
+		width: 25%;
+	}
+	#thirdbutton{
+		width: 20%;
+	}
+	.sizelist{
+		width: 80%;
+	}
+	.row{
+		width: 145%;
+	}
+	
+</style>
+<div class="sizelist">
+    <div class="col-sm-4">
+	<ul class="list-group">
+  <li class="list-group-item active">
+    <span class="badge">14</span>
+    fgjfjdfh
+  </li>
+  <li class="list-group-item">
+    <span class="badge">3</span>
+    Cwertyuias
+  </li>
+  <li class="list-group-item">
+    <span class="badge">4</span>
+    aaaaaaaaaaaaaa
+  </li>
+  <li class="list-group-item">
+    <span class="badge">1</span>
+    xxxxxxxxxxxxxxxx
+  </li>
+</ul>	
+</div>
+</div>
+<div class="col-sm-8">
+
+	<div class="filter">
+	<div class="col-sm-6">
+		<div class="input-group">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Search</button>
+      </span>
+      <input type="text" class="form-control" placeholder="Search for...">
+    </div>
+    </div>
+    <!-- /input-group -->
+    <div class="row">
+    <div class="col-lg-4">
+     
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Sort by...">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Sort</button>
+      </span>
+    </div><!-- /input-group -->
+
+</div>
+ 
+  </div>
+<br>
+</div>
+	
+	<div class="col-sm-4">
+		
+			<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			</div>
+		
+	</div>
+	<div class="col-sm-4">
+		
+		<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			</div>
+		
+	</div>
+	<div class="col-sm-4">
+		
+		<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			</div>
+		
+	</div>
+	<div class="col-sm-4">
+		
+			<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			
+		</div>
+	</div>
+	<div class="col-sm-4">
+		
+		<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			
+		</div>
+	</div>
+	<div class="col-sm-4">
+		
+		<div class="picturebox">
+			<a href="lorem.html"><img src=".." 
+  			width="196" height="200" alt="lorem"></a>
+			</div>
+			<div class="textbox">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+			</div>
+			<div class="buttons">
+				<p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+			</div>
+		
+	</div>
 </div>
