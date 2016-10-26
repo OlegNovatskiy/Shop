bbdb8a405fcf46da4d4cf4542cf0e46b5cbc23e4
olegnovatskiy <olegnovatskiy@gmail.com>
2016-10-26 09:06:56 +0300

    first conflict

--- views/products/index.php
+++ views/products/index.php
@@ -12,37 +12,77 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <!--Type code here-->
 
-    <table>
-        <thead>
-        <th>
-            <td>Назва</td>
-            <td>Ціна</td>
-            <td>ата створення</td>
-        </th>
-        </thead>
-        <tbody>
+<h3>FEATURED</h3>
+    <style type="text/css">
+        p{	margin-left: 4px;
+            margin-right: 4px;
 
-
-    <?php
-        foreach($data['products'] as $product){
-            ?>
-            <tr>
-                <td><?php
-                    foreach($data['producer'] as $producer){
-                        if($producer['id_producer']==$product['maker']){
-                            echo $producer['name'];
-                            break;
-                        }
-                    }
-                    echo " ".$product['version'];
-                    ?></td>
-                <td><?php echo $product['price'];?></td>
-                <td><?php echo date("dd mm yy",$product['price']);?></td>
-            </tr>
-    <?php
         }
-    ?>
-        </tbody>
-    </table>
-    Index
-</>
+        img{
+            margin-left: 4px;
+            margin-right: 4px;
+        }
+        #mainbutton{
+            width: 38%;
+        }
+        #secondbutton{
+            width: 20%;
+        }
+        #thirdbutton{
+            width: 35%;
+>>>>>>> ba98b3fa64981599f60312a169be06ab81e02ba7
+        }
+    </style>
+
+
+    <div class="col-xs-6 col-sm-8 col-lg-12">
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Add to cart</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">Compare</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+                <div class="picturebox">
+                    <a href="lorem.html"><img src=".." width="196" height="200" alt="lorem"></a>
+                </div>
+                <div class="textbox">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                </div>
+                <div class="buttons">
+                    <p><a href="#" class="btn btn-primary" id="mainbutton" role="button">Button</a> <a href="#" class="btn btn-default" id="secondbutton" role="button">Like!</a><a href="#" class="btn btn-default" id="thirdbutton" role="button">IN</a>
+                </div>
+        </div>
+    </div>
+
+
+
+
+</div>
